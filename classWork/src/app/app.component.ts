04926beb1950613b5de3fc5972eb82f9886e7c54
lasha <maisuradze.llasha@gmail.com>
2025-04-12 18:49:01 +0400

    classwork

--- classWork/src/app/app.component.ts
+++ classWork/src/app/app.component.ts
@@ -9,4 +9,51 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'classWork';
+
+  name = 'lasha'
+  mail= 'mail@test.com'
+  pass = 'las123'
+  btn = 'show'
+  name2 = ''
+  mail2 = ''
+  pass2 = ''
+
+
+
+
+
+
+
+  
+
+  fun(){
+    if(this.name2 == '' && this.mail2 == '' && this.pass2 == ''){
+    this.name2 = this.name
+    this.mail2 = this.mail 
+    this.pass2 = this.pass  
+    this.btn = 'clear'}
+    else if(this.btn=='clear'){
+      this.name2 = ''
+      this.mail2 = '' 
+      this.pass2 = ''
+      this.btn = 'show'  
+    }
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
