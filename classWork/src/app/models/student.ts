c1b6001af26016f7f68d1dc96585469b1079272d
lasha <maisuradze.llasha@gmail.com>
2025-04-22 19:09:32 +0400

    st

---
+++ classWork/src/app/models/student.ts
@@ -0,0 +1,10 @@
+ export class Student {
+    name? : string; 
+    age? : number; 
+    email? : string;
+    address? : {
+        city? : string;
+        street? : string;
+        zip? : number;
+    }
+}
